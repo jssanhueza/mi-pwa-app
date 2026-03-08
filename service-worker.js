0a5737e785c48e5146563887b4ffadea359c8f42
jssanhueza <jssanhueza@gmail.com>
2026-03-08 00:29:49 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,25 @@
-const CACHE = "mi-app-v2";
+const CACHE = "mi-pwa-cache-v1";
 
-self.addEventListener("install", e => {
-  self.skipWaiting();
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", e => {
-  clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
