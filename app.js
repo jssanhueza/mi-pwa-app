5c2ba3d6ee9c4bb8f9a1041000cb6c13831caa22
jssanhueza <jssanhueza@gmail.com>
2026-03-08 00:40:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js");
-}
-
-function obtenerDatos(){
-
-fetch("AQUI_VA_TU_APPS_SCRIPT")
-
-.then(r => r.json())
-
-.then(data => {
-document.getElementById("resultado").textContent =
-JSON.stringify(data,null,2);
-});
-
+  navigator.serviceWorker.register("./service-worker.js");
 }
