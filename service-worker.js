9f4e5adfec0500a2d206cc20792843f9c92df28a
jssanhueza <jssanhueza@gmail.com>
2026-03-08 00:39:45 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,13 @@
-const CACHE_NAME = 'v1_mi_pwa';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = "mi-pwa-v1";
 
-// Instalación del Service Worker
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting());
-      })
-  );
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-// Activación
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
-// Evento Fetch (Crucial para que Chrome vea que es una PWA)
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
